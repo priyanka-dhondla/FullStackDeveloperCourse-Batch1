fbce9e04b5f8f44a3d130a3e0133637163baffdc
priyanka-dhondla <164379280+priyanka-dhondla@users.noreply.github.com>
2024-11-14 20:54:52 +0530

    All days codes added

---
+++ Day-11/basic.js
@@ -0,0 +1,133 @@
+// variables : Variables are used to store the values.
+
+// 3 types of variables
+
+// var,let,const
+
+// var a=20;
+
+// let b="JavaScript";
+
+// const c=true;
+
+// console.log(a)
+
+// console.log(b)
+
+// console.log(c)
+
+// Datatypes -  2 types
+// 1.Primitive
+// 2.non primitive
+
+
+// Examples for Primitive data types are : - number, string,boolean, undefined, null
+
+//primitive data types are immutable
+
+//They can hold single value
+
+//Non - primitive data types : - Arrays, functions and objects
+
+//non primitive datatypes are mutable
+
+//These are used to store multiple values.
+
+//number
+
+// var x=10.5
+
+// console.log(x)
+// console.log(typeof(x))
+
+//string
+
+const str="Frontend Developer";
+// console.log(ab)
+// console.log(typeof(ab))
+
+// let newStr=str.toUpperCase();
+// let lowerStr=str.toLowerCase();
+// console.log(lowerStr)
+// console.log(newStr)
+
+// const name="Venkat"
+// console.log(name.length)
+
+// console.log(name[5])
+
+// var y="Hello"
+
+// console.log(y)
+
+//Boolean 
+
+// let isTrue=true;
+// console.log(isTrue)
+// console.log(typeof(isTrue))
+
+// let isTrue=false;
+// console.log(isTrue)
+// console.log(typeof(isTrue))
+
+//undefined
+
+// var a;
+// console.log(a)
+// console.log(typeof(a))
+
+//Null
+
+// var b=null;
+// console.log(b)
+// console.log(typeof(b))
+
+// Non-primitive datatypes
+
+// Array
+
+var array=[10,20,"hello",true]
+// console.log(array)
+// console.log(typeof(array))
+
+// var arr1=[10,20,30,40]
+
+// var arr2=["Vishal","Krsishna","Venkat","Dharun","Dinesh"]
+
+// var arr3=[true,false]
+
+// console.log(arr1)
+// console.log(arr2)
+// console.log(arr3)
+
+let marks=[12,34,23,56,98,76]
+console.log(marks[2])
+
+console.log(marks[4])
+
+console.log(marks[5])
+
+marks[-5]=80;
+
+console.log(marks)
+
+
+//slice
+
+//splice
+
+//push()
+
+//pop()
+
+//shift()
+
+//unshift()
+
+// concat()
+
+// var a; //variable declaration
+
+// a=10; //assignement
+
+// console.log(a)
